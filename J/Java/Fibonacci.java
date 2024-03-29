44e869855f1bb2aafc5c2eae268beecc8a94dc64
Лука <164436043+diamond-ore@users.noreply.github.com>
2024-03-29 14:26:14 -0400

    Java
    Changed it to output the first 10 digits of the Fibonacci sequence

--- J/Java/Fibonacci.java
+++ J/Java/Fibonacci.java
@@ -1,7 +1,7 @@
 public class Fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
-        for (int i = 1; i<15; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(a);
             b += a;
             a = b - a;
