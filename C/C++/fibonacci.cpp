880438f18721fe5e054bf8df84c2f716fa99180f
Лука <164436043+diamond-ore@users.noreply.github.com>
2024-03-29 13:52:03 -0400

    C++
    Changed numbers to output the first 10 digits instead

--- C/C++/fibonacci.cpp
+++ C/C++/fibonacci.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     int a = 0, b = 1;
-    for (int i = 1; i<15; i++) {
+    for (int i = 0; i < 10; i++) {
         std::cout << std::to_string(a)+"\n";
         b += a;
         a = b - a;
