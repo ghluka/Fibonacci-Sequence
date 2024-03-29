43d46a15d1b941fe59f48713b6c8f75784cd34ff
Лука <164436043+diamond-ore@users.noreply.github.com>
2024-03-29 13:51:39 -0400

    C#
    Changed numbers to output the first 10 digits instead

--- C/C#/Fibonacci.cs
+++ C/C#/Fibonacci.cs
@@ -4,7 +4,7 @@ namespace Fibonacci {
     public class Fibonacci {
         public static void Main(string[] args) {
             int a = 0, b = 1;
-            for (int i = 1; i<15; i++) {
+            for (int i = 0; i < 10; i++) {
                 Console.WriteLine($"{a}");
                 b += a;
                 a = b - a;
