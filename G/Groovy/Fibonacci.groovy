c5280298dbf5bf95d282a45e26350f8f789c269f
Лука <164436043+diamond-ore@users.noreply.github.com>
2024-03-29 14:20:11 -0400

    Groovy
    Changed it to output the first 10 digits of the Fibonacci sequence

--- G/Groovy/Fibonacci.groovy
+++ G/Groovy/Fibonacci.groovy
@@ -1,8 +1,8 @@
 class Fibonacci {
     static void main(String[] args) {
         def a = 0, b = 1;
-        for (def i = 1; i<15; i++) {
-            System.out.println(a);
+        for (def i = 0; i < 10; i++) {
+            println(a);
             b += a;
             a = b - a;
         }
