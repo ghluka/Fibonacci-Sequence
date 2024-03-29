1612dc56186b405bb0c9f2a1443bbcbc1b2a1a93
Лука <164436043+diamond-ore@users.noreply.github.com>
2024-03-29 14:17:51 -0400

    Fortran
    Changed it to output the first 10 digits of the Fibonacci sequence

--- F/Fortran77/fibonacci.f
+++ F/Fortran77/fibonacci.f
@@ -1,10 +1,9 @@
-c fortran77 fibonacci
-      program fibonacci
-            a = 0
-            b = 1
-            do i = 0, 15
-                  print *, a
-                  b = b + a
-                  a = b - a
-            end do
-      end program fibonacci
+program fibonacci
+     a = 0
+     b = 1
+     do i = 1, 10
+           print *, a
+           b = b + a
+            a = b - a
+     end do
+end program fibonacci
--- F/Fortran90/fibonacci.f
+++ F/Fortran90/fibonacci.f
@@ -1,9 +1,8 @@
-! fortran90 fibonacci
 PROGRAM FIBONACCI
 INTEGER a, b, i
 a = 0
 b = 1
-DO i = 0, 15
+DO i = 1, 10
    PRINT *, a
    b = b + a
    a = b - a
