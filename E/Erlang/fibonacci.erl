1a2aff3819d50257544c3986d26f6fe42d67172d
Лука <164436043+diamond-ore@users.noreply.github.com>
2024-03-29 14:13:58 -0400

    Erlang
    I'm not good at Erlang, in-fact I don't know Erlang at all. I tried making my old code a lot better and I think I succeeded.

--- E/Erlang/fibonacci.erl
+++ E/Erlang/fibonacci.erl
@@ -1,19 +1,15 @@
--module(fibonacci).
--export([while/1,fib/1,while/2, start/0]). 
-
-fib(X) when X < 2 ->
+fib(X) when X < 1 ->
+     0;
+fib(X) when X == 1 ->
      1;
 fib(X) when X >= 2 ->
      fib(X-1) + fib(X-2).
 
 while(L) -> while(L,0). 
-while([], Acc) -> Acc;
+while([], X) -> X;
+while([_|T], X) ->
+    io:fwrite("~w~n",[fib(X)]), 
+    while(T,X+1). 
 
-while([_|T], Acc) ->
-    io:fwrite("~w~n",[fib(Acc-1)]), 
-    while(T,Acc+1). 
-    
-start() -> 
-    % idk how to make a proper loop so this will suffice i guess
-    X = [2,3,4,5,6,7,8,9,10,11,12,13,14,15], 
-    while(X).
+main(_) ->
+    while(lists:seq(1, 10)).
