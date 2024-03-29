bf7f77786750191872538e3a2078ae1e5af334d9
Лука <164436043+diamond-ore@users.noreply.github.com>
2024-03-29 14:29:12 -0400

    Objective-C
    Changed it to output the first 10 digits of the Fibonacci sequence

--- O/ObjectiveC/fibonacci.mm
+++ O/ObjectiveC/fibonacci.mm
@@ -2,7 +2,7 @@
 
 int main (int argc, const char * argv[]) {
     int a = 0, b = 1;
-    for (int i = 1; i<15; i++) {
+    for (int i = 0; i < 10; i++) {
         printf("%u\n", a);
         b += a;
         a = b - a;
